a4f7bd14184925ac03b0c01cdcbd5bc2ceabd6bd
Jeanne Pietropaoli <j.pietropaoli@yahoo.fr>
2022-04-25 21:10:55 -0400

    Add animated gradient to submitbtn if input=valid

--- script.js
+++ script.js
@@ -1,6 +1,42 @@
 const LABELS = document.querySelectorAll('label');
+const REQUIREDINPUTS = document.querySelectorAll('input[required]');
+const NAMEINPUT = document.querySelector('input[name="userFirstName"]');
+const LASTNAMEINPUT = document.querySelector('input[name="userLastName"]');
+const EMAILINPUT = document.querySelector('input[name="userEmail"]');
+const PASSWORD = document.querySelector('input[name="userPassword"]');
+const PASSWORDCHECK = document.querySelector('input[name="confirmPassword"]');
+
+
+const SUBMITBTN = document.querySelector('button');
 
 LABELS.forEach(label => {
     let labelength = label.innerHTML.length;
     label.style.backgroundSize = `${labelength-38}px`;
 })
+
+REQUIREDINPUTS.forEach(requiredInput =>{
+    requiredInput.addEventListener('input', function(){
+        if (NAMEINPUT.checkValidity() && LASTNAMEINPUT.checkValidity() && EMAILINPUT.checkValidity() && PASSWORD.checkValidity() && PASSWORD.value===PASSWORDCHECK.value){
+            if (!(SUBMITBTN.classList.contains('addGradient'))){
+                SUBMITBTN.classList.add('addGradient');
+            }
+            
+        }
+        else{
+            if (SUBMITBTN.classList.contains('addGradient')){
+                SUBMITBTN.classList.remove('addGradient');
+            }
+        }
+    })
+})
+
+
+
+
+/* if (requiredInput.checkValidity()){
+    requiredInput.style.outlineColor = 'green';
+    }
+else {
+    requiredInput.style.outlineColor = 'red';
+    allValidInputs = false;
+} */
